e2c3401352ffb78ef9c2c6f9661f6cf8cf7021af
Zhuangye Luo <1738845246@qq.com>
2017-09-16 02:57:32 -0700

    xblog

---
+++ config/social.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+/**
+ * Created by PhpStorm.
+ * User: lufficc
+ * Date: 2016/8/21
+ * Time: 22:47
+ */
+
+
+    'weibo'    => [
+        'url'  => 'http://weibo.com/u/6004112636',
+        'icon'   => 'weibo',
+    ],
+    'github'  => [
+        'url' =>'https://github.com/LuoJohnYeah',
+        'icon'   => 'github',
+    ],
+
+];
